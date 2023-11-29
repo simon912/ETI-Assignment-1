60cff7e21793b061011c7ec0aba128ff5d1e23bb
simon912 <s10228098@connect.np.edu.sg>
2023-11-29 09:00:17 +0800

    Made some adjustment to the frontend

--- frontend/home/home.go
+++ frontend/home/home.go
@@ -26,8 +26,13 @@ const homeTemplate = `
 	#changeCarOwnerContainer {
 		display: none;
 	}
+	#viewProfileContainer {
+		display: none;
+	}
 </style>
  <script>
+ 		const urlParams = new URLSearchParams(window.location.search);
+		const username = urlParams.get('username');
 		window.onload = function () {
 			retrieveUserData();
 		}
@@ -40,30 +45,31 @@ const homeTemplate = `
                 messageContainer.style.display = 'none';
             }, 3000);
         }
-		function redirectedToProfile() {
-			const urlParams = new URLSearchParams(window.location.search);
-        	const username = urlParams.get('username');
-        
-        // Make a GET request to the server to handle the redirection
-        fetch('/redirect-profile?username=' + encodeURIComponent(username))
-            .then(response => {
-                if (response.ok) {
-                    // Redirect to the profile page
-                    window.location.href = '/profile?username=' + encodeURIComponent(username);
-                } else {
-                    console.error('Error handling redirection:', response.status, response.statusText);
-                }
-            })
-            .catch(error => {
-                console.error('Error handling redirection:', error);
-            });
+		function showProfileContainer() {
+			document.getElementById('viewProfileContainer').style.display = 'block';
 		}
 		function logOutUser() {
-
+			fetch('http://localhost:5000/api/v1/logout', {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			.then(response => {
+				if (response.ok) {
+					// Redirect to the login page
+					window.location.href = 'http://localhost:5001/index';
+				} else {
+					throw new Error('Logout failed');
+				}
+			})
+			.catch(error => {
+				// Display an error message or handle the error appropriately
+				showMessage('Logout failed.');
+			});
 		}
 		function retrieveUserData() {
-			const urlParams = new URLSearchParams(window.location.search);
-			const username = urlParams.get('username');
+			
 		
 			// Send a GET request to /api/v1/user/{username} endpoint for user data
 			fetch('http://localhost:5000/api/v1/user/' + username, {
@@ -99,13 +105,138 @@ const homeTemplate = `
 				showMessage('Failed to retrieve user data.');
 			});
 		}
+		// For Profile
+		function showUserInfo() {
+			// Send a GET request to /api/v1/user/{username} endpoint for user data
+			fetch('http://localhost:5000/api/v1/user/' + username, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			})
+			.then(response => {
+				if (response.ok) {
+					return response.json();
+				} else {
+					throw new Error('Failed to retrieve user data');
+				}
+			})
+			.then(data => {
+				// Assuming the backend sends JSON data with user details
+				console.log('Response Data:', data);
+				// Extract usergroup, firstname, and lastname from the data
+				const usergroup = data['User Group'];
+				const firstname = data['First Name'];
+				const lastname = data['Last Name'];
+				const mobileno = data['Mobile Number'];
+				const emailaddr = data['Email Address'];
+				document.getElementById('updateUsername').value = username;
+				document.getElementById('updateMobileNo').value = mobileno;
+				document.getElementById('updateEmailAddr').value = emailaddr;
+			})
+			.catch(error => {
+				// Display an error message or handle the error appropriately
+				showMessage('Failed to retrieve user data.');
+			});
+			const updateUserContainer = document.getElementById('updateUserContainer');
+			const changeCarOwnerContainer = document.getElementById('changeCarOwnerContainer');
+			updateUserContainer.style.display = 'block';
+			changeCarOwnerContainer.style.display = 'none';
+            document.getElementById('updateUserForm').style.display = 'block';
+		}
+		function showChangeCarOwner() {
+			const updateUserContainer = document.getElementById('updateUserContainer');
+			const changeCarOwnerContainer = document.getElementById('changeCarOwnerContainer');
+			updateUserContainer.style.display = 'none';
+			changeCarOwnerContainer.style.display = 'block';
+
+			document.getElementById('carownerLicenseNo').value = '';
+			document.getElementById('carownerCarPlateNo').value = '';
+            document.getElementById('changeCarOwnerForm').style.display = 'block';
+		}
+		function updateUserInfo(username) {
+            const mobilenumber = parseInt(document.getElementById('updateMobileNo').value);
+            const emailaddr = document.getElementById('updateEmailAddr').value;
+        
+            fetch('http://localhost:5000/api/v1/updateuser/' + username, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "Mobile Number": mobilenumber,
+                    "Email Address": emailaddr,
+                }),
+            })
+            .then(response => {
+                if (response.ok) {
+                    document.getElementById('updateUserForm').style.display = 'none';
+                    document.getElementById('message').style.display = 'block';
+                    document.getElementById('message').textContent = "User " + username + "'s info updated successfully";
+                } else {
+                    throw new Error('User update failed');
+                }
+            })
+            .catch(error => {
+                console.error('Error updating user:', error.message);
+            });
+        }
+		function changeCarOwner(username) {
+            const licenseno = parseInt(document.getElementById('carownerLicenseNo').value);
+            const plateno = document.getElementById('carownerCarPlateNo').value;
+            fetch('http://localhost:5000/api/v1/changecarowner/' + username, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "License Number": licenseno,
+                    "Plate Number": plateno,
+                }),
+            })
+            .then(response => {
+                if (response.ok) {
+                    document.getElementById('changeCarOwnerForm').style.display = 'none';
+                    document.getElementById('message').style.display = 'block';
+                    document.getElementById('message').textContent = "User " + username + " changed to Car Owner";
+                } else {
+                    throw new Error('User update failed');
+                }
+            })
+            .catch(error => {
+                console.error('Error updating user:', error.message);
+            });
+        }
 	</script>
 </head>
 <body>
 	<div id="userButtonList">
-	<button type="button" onclick="redirectedToProfile()">View Profile</button>
+	<button type="button" onclick="showProfileContainer()">View Profile</button>
 	<button type="button" onclick="redirectedToProfile()">View Trips</button>
 	<button type="button" onclick="logOutUser()">Log Out</button>
+	</div>
+	<div id="viewProfileContainer">
+		<button type="button" onclick="showChangeCarOwner()">Change to Car Owner</button>
+		<button type="button" onclick="showUserInfo()">Update Profile</button>
+		<div id="changeCarOwnerContainer">
+			<form id="changeCarOwnerForm">
+				<label for="carownerLicenseNo">Your Driver's License Number:</label>
+				<input type="text" id="carownerLicenseNo" required><br>
+				<label for="carownerCarPlateNo">Your Car Plate Number:</label>
+				<input type="text" id="carownerCarPlateNo" required><br>
+				<button type="button" onclick="changeCarOwner(username)">Change to Car Owner</button>
+			</form>
+		</div>
+		<div id="updateUserContainer">
+			<form id="updateUserForm">
+				<label for="updateUsername">Username:</label>
+				<input type="text" id="updateUsername" readonly><br>
+				<label for="updateMobileNo">Mobile Number:</label>
+				<input type="text" id="updateMobileNo" required><br>
+				<label for="updateEmailAddr">Email Address:</label>
+				<input type="text" id="updateEmailAddr" required><br>
+				<button type="button" onclick="updateUserInfo(username)">Update</button>
+			</form>
 	</div>
     <div id="message"></div>
 </body>
@@ -136,13 +267,10 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	http.HandleFunc("/profile", profile.ProfileHandler)
-	http.HandleFunc("/display-profile", func(w http.ResponseWriter, r *http.Request) {
-		username := r.URL.Query().Get("username")
-		redirectToProfileSuccess(w, r, username)
-	})
-
 }
-func redirectToProfileSuccess(w http.ResponseWriter, r *http.Request, username string) {
-	http.Redirect(w, r, "http://localhost:5001/profile?username="+username, http.StatusSeeOther)
+func init() {
+	http.HandleFunc("/redirect-profile", func(w http.ResponseWriter, r *http.Request) {
+		// You may need to import the "profile" package if it's not already imported
+		profile.ProfileHandler(w, r)
+	})
 }
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"eti-assignment-1/frontend/profile"
 	"fmt"
 	"log"
 	"net/http"
@@ -43,6 +44,7 @@ func main() {
 	corsOptions := cors.New(cors.Options{
 		AllowedOrigins: []string{"http://localhost:5001"},
 		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders: []string{"Content-Type"},
 	})
 	handler := corsOptions.Handler(router)
 
@@ -50,6 +52,8 @@ func main() {
 	//This GET method retrieves the relevant course information.
 	// For Login
 	router.HandleFunc("/api/v1/login/{username}", GetUser).Methods("GET")
+	router.HandleFunc("/api/v1/logout", LogoutHandler).Methods("GET", "PUT", "POST")
+
 	//test case: curl http://localhost:5000/api/v1/user/naruto55 -X POST -d "{\"User Group\":\"Car Owner\", \"First Name\":\"Naruto\", \"Last Name\":\"Uzumaki\", \"Mobile Number\":99987634, \"Email Address\":\"naruto@gmail.com\"}"
 	// For Register
 	router.HandleFunc("/api/v1/register/{username}", CreateUser).Methods("POST")
@@ -57,13 +61,37 @@ func main() {
 	router.HandleFunc("/api/v1/updateuser/{username}", UpdateUser).Methods("PUT")
 	// test case: curl http://localhost:5000/api/v1/changecarowner/naruto55 -X PUT -d "{\"License Number\": 111123335, \"Plate Number\": \"ABC123\"}"
 	router.HandleFunc("/api/v1/changecarowner/{username}", ChangeToCarOwner).Methods("PUT")
+
+	http.HandleFunc("/profile", profile.ProfileHandler)
+	http.HandleFunc("/display-profile", func(w http.ResponseWriter, r *http.Request) {
+		username := r.URL.Query().Get("username")
+		redirectToProfileSuccess(w, r, username)
+	})
+	router.HandleFunc("/redirect-profile/", RedirectProfileHandler).Methods("GET")
 	// handlefunc here which deletes account after its 1 year old/365 days old
 
 	// Endpoint for Car-Pooling Trips
 	//router.HandleFunc("/api/v1/carpoolingtrip", GetAllTrip).Methods("GET")
 	//router.HandleFunc("/api/v1/carpoolingtrip/{tripid}", PublishTrip).Methods("POST")
+	router.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5001")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusNoContent)
+	})
 	fmt.Println("Listening at port 5000")
 	log.Fatal(http.ListenAndServe(":5000", handler))
+}
+func redirectToProfileSuccess(w http.ResponseWriter, r *http.Request, username string) {
+	http.Redirect(w, r, "http://localhost:5001/profile?username="+username, http.StatusSeeOther)
+}
+func RedirectProfileHandler(w http.ResponseWriter, r *http.Request) {
+	// Extract the username from the query parameters
+	username := r.URL.Query().Get("username")
+
+	// Redirect to the profile page
+	http.Redirect(w, r, "/profile?username="+username, http.StatusSeeOther)
 }
 
 // Helper function to handle null values in SQL parameters
@@ -74,7 +102,7 @@ func nullOrValue(value interface{}) interface{} {
 	return value
 }
 
-// Custom unmarshal function for LicenseNo and PlateNo
+// Custom unmarshal function for LicenseNo
 func (u *Users) UnmarshalJSON(data []byte) error {
 	type Alias Users
 	aux := &struct {
@@ -148,6 +176,24 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("User doesn't exist")
 		http.Error(w, "User not found", http.StatusNotFound)
 	}
+}
+
+func LogoutHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received logout request")
+	if r.Method == "OPTIONS" {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5001")
+		w.Header().Set("Access-Control-Allow-Methods", "GET")
+		// Add other CORS headers as needed
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	// Respond to the actual request (GET)
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5001")
+	// Add other CORS headers as needed
+
+	// Perform the redirect after the actual request is handled
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
 func userExists(username string) bool {
@@ -240,6 +286,7 @@ func ChangeToCarOwner(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	username := params["username"]
 	var updateUser Users
+	fmt.Println("Received JSON:", r.Body)
 	err := json.NewDecoder(r.Body).Decode(&updateUser)
 	if err != nil {
 		panic(err.Error())
